c0aeffd82a072152e2c67bd991b5f0f22ab95de1
PanasheCodesAlot <118339390+PanasheCodesAlot@users.noreply.github.com>
2023-09-30 16:57:12 +0100

    Update +page.svelte 03

--- src/routes/admin/searchResults/+page.svelte
+++ src/routes/admin/searchResults/+page.svelte
@@ -57,7 +57,7 @@
 
 	//Redirects to view event
 	const viewEvt = (evt) => {
-		const url = `events/view/?event=${JSON.stringify(evt)}`
+		const url = `events/view/?event=${encodeURIComponent(JSON.stringify(evt))}`
 		redirectTo(url)
 	}
 
